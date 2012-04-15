43a23ccfc768b992bcb4c3bc57a360cdc3e3f9aa
Galina Rybatsky <rybag047@newschoool.edu>
2012-04-15 19:03:12 -0400

    Assignment 5: with ReviewViewController

---
+++ Restaurants/ReviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReviewViewController.h
+//  Restaurants
+//
+//  Created by Galina Rybatsky on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewViewController : UITableViewController
+
+@end
---
+++ Restaurants/ReviewViewController.m
@@ -0,0 +1,13 @@
+//
+//  ReviewViewController.m
+//  Restaurants
+//
+//  Created by Galina Rybatsky on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReviewViewController.h"
+
+@implementation ReviewViewController
+
+@end
